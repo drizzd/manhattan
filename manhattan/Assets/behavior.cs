fadf7fa00b1e0b566b5baf92be50906f4e9c6b77
Jonas Demski <demski@email>
2016-01-29 21:37:26 +0100

    import

---
+++ manhattan/Assets/behavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class behavior : MonoBehaviour {
+	public int speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(1, 2, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + new Vector3(1, 0, 0) * speed * Time.deltaTime;
+	}
+}
---
+++ manhattan/Assets/follower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follower : MonoBehaviour {
+	public GameObject leader;
+	// Use this for initialization
+	void Start () {
+		leader= GameObject.FindGameObjectWithTag("Leader");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
