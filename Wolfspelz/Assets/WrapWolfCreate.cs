ccf896c2c89b3687f2005bde4947c81e1f71b7f5
Matthias Babl <m_babl@web.de>
2016-01-30 15:06:47 +0100

    Wolfspelz

---
+++ Wolfspelz/Assets/WrapWolfCreate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WrapWolfCreate : MonoBehaviour {
+	public float horz;
+	public float vert;
+	public Transform DummyWolf;
+
+	// Use this for initialization
+	void Start () {
+		vert = Camera.main.orthographicSize;
+		horz = Camera.main.aspect * vert;
+		Transform clone;
+
+
+		clone = Instantiate(DummyWolf,transform.position + new Vector3(-horz*2,0,0),Quaternion.identity) as Transform;
+		clone.parent = transform;
+		clone = Instantiate(DummyWolf,transform.position + new Vector3(horz*2,0,0),Quaternion.identity) as Transform;
+		clone.parent = transform;
+		clone = Instantiate(DummyWolf,transform.position + new Vector3(0,-vert*2,0),Quaternion.identity) as Transform;
+		clone.parent = transform;
+		clone = Instantiate(DummyWolf,transform.position + new Vector3(0,vert*2,0),Quaternion.identity) as Transform;
+		clone.parent = transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
